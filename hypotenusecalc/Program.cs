24aa8aa7740e4baf5c2c62dbeff98b45c7935760
oliviadebelack <debelaom@grace.edu>
2020-09-03 18:05:42 -0400

    Formula Entered

--- hypotenusecalc/Program.cs
+++ hypotenusecalc/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace hypotenusecalc
+namespace trianglehw
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double side1 = 0.0;
+            double side2 = 0.0;
+            double hypt;
+            string temp;
+
+            Console.WriteLine("Calculate The Hypotenuse Of Your Right Triangle. Press Enter to Begin! ");
+            //read in side 1
+            temp = Console.ReadLine();
+            side1 = getInt("Enter the Length of Side 1: ");
+
+
+            //read in the gallons
+            Console.WriteLine("Enter the Length of Side 2: ");
+            temp = Console.ReadLine();
+            side2 = Double.Parse(temp);
+
+            //compute mpg
+            hypt = Math.Sqrt(side1 * side1 + side2 * side2);
+            Console.WriteLine("The Hypotenuse of Your Triangle Is " + hypt);
+
         }
+        public static int getInt(string prompt)
+        {
+            string temp;
+            int value;
+            Console.WriteLine(prompt);
+            temp = Console.ReadLine();
+            value = Int32.Parse(temp);
+            return value;
+
+
+        }
+
     }
 }
